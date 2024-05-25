900576b773da889bb10f5c54ad78305b858e6c0c
kai993 <huf0618@icloud.com>
2024-05-25 13:25:29 +0900

    束縛と環境

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"monkey-go/evaluator"
 	"monkey-go/lexer"
+	"monkey-go/object"
 	"monkey-go/parser"
 )
 
@@ -28,6 +29,7 @@ const (
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -46,7 +48,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
